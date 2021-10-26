cb78568f27be3d89fffe7e15faf7c5c2f78bf01a
Ashok Nath <nathashok87@gmail.com>
2021-10-26 22:46:58 +1000

    Added ui

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Local Files
+import PostList from './PostList';
+
+const App = () => {
+    return(
+        <div className="ui container">
+           <PostList/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/PostList.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions'
+
+class PostList extends Component{
+
+    componentDidMount(){
+        this.props.fetchPost();
+    }
+
+    renderList() {
+        return this.props.posts.map(post => {
+            return(
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user"/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    render(){
+       return(
+            <div className="ui releaxed divided list">
+                {this.renderList()}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {posts : state.posts};
+};
+
+
+export default connect(mapStateToProps,{fetchPost})(PostList);
